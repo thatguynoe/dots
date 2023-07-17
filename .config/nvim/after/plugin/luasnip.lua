462a5bdabe82b7fdd720343908057b0de25d23c6
thatguynoe <noereyesrivas@gmail.com>
2023-07-17 14:38:13 -0700

    move snippets from lua to snipmate format
    Add function to modify snippets according to file type.

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,24 +1,29 @@
-local luasnip = require("luasnip")
+HOME = os.getenv("HOME")
 
-luasnip.config.set_config {
+-- load snippets from the ~/.config/nvim/snippets/ directory for the corresponding language
+require("luasnip/loaders/from_snipmate").lazy_load({
+    path = {HOME .. "/.config/nvim/snippets"}
+})
+
+-- command to open the snippet file that belongs to the language you are editing
+vim.cmd([[
+function SnippetsEdit()
+    execute ":e ~/.config/nvim/snippets/" . &filetype . ".snippets"
+endfunction
+command! SnippetsEdit call SnippetsEdit()
+]])
+
+local ls = require("luasnip")
+
+ls.config.set_config({
     -- This tells LuaSnip to remember to keep around the last snippet.
     -- You can jump back into it even if you move outside of the selection
     history = true,
 
+    store_selection_keys = "<c-s>",
+
     -- Updates dynamic snippets as you type
     updateevents = "TextChanged,TextChangedI",
-}
-
--- luasnip.parser.parse_snippet(<text>, <VS****> style snippet>)
-luasnip.add_snippets(nil, {
-    tex = {
-        -- LaTeX specific snippets go here.
-        luasnip.parser.parse_snippet("b", "\\begin{$1}\n    $0\n\\end{$1}"),
-    },
-
-    markdown = {
-        -- Markdown specific snippets go here.
-        luasnip.parser.parse_snippet("**", "**$1** $0"),
-        luasnip.parser.parse_snippet("*", "*$1* $0"),
-    },
+    region_check_events = "CursorMoved",
+    delete_check_events = "TextChanged"
 })
