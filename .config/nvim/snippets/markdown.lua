5ead6a15d88fe9021746485becab8e8df9996525
thatguynoe <noereyesrivas@gmail.com>
2025-08-22 17:39:36 -0400

    remove extra space in markdown snippets

--- .config/nvim/snippets/markdown.lua
+++ .config/nvim/snippets/markdown.lua
@@ -3,11 +3,11 @@ local ls = require("luasnip")
 return {
     -- Bold text
     s({ trig = "**", name = "Bold", dscr = "Insert bold text" },
-      fmt("**{}** {}", { i(1), i(0) })
+      fmt("**{}**{}", { i(1), i(0) })
     ),
 
     -- Italic text
     s({ trig = "*", name = "Italic", dscr = "Insert italic text" },
-      fmt("*{}* {}", { i(1), i(0) })
+      fmt("*{}*{}", { i(1), i(0) })
     ),
 }
